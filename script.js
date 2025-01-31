0650e00e2e56ac4954a718a47305f40f47c3f80e
Henry Odunsi <henryodunsi05@gmail.com>
2025-01-31 21:43:50 +0100

    mode

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function updateTime() {
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+const check = document.getElementById("checkbox");
+let container = document.getElementById("contain");
+
+check.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+  container.classList.toggle("dark");
+  //   container.classList.remove("container1");
+});
